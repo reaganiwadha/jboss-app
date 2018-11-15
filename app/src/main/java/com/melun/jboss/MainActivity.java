2bf543d9d404724b9bba49ad0509af74e081ef54
reaganiwadha <reaganiwadha@gmail.com>
2018-11-15 09:30:57 +0700

    working net

--- app/src/main/java/com/melun/jboss/MainActivity.java
+++ app/src/main/java/com/melun/jboss/MainActivity.java
@@ -1,20 +1,46 @@
 package com.melun.jboss;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+    public String giturl = "https://api.github.com/orgs/JBossOutreach/repos";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OkHttpClient client = new OkHttpClient();
 
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(giturl).get().build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                Log.d("JSON",json);
+            }
+        });
 
 
 
     }
+
 }
